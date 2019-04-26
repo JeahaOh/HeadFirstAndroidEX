97cc5cc3a6da22b666fdaad7f4a459370cb29d09
JeahaOh <jeaha1217@gmail.com>
2019-04-26 17:45:07 +0900

    .

---
+++ 10_Dynamic_Fragment/Workout/app/src/main/java/ex/android/workout/WorkoutDetailFragment.java
@@ -0,0 +1,58 @@
+package ex.android.workout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+//  Fragment를 상속 받음
+    private long workoutId;
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        if( savedInstanceState != null ) {
+            workoutId = savedInstanceState.getLong( "workoutId" );
+        }
+    }
+
+    //  onCreateView() 프래그먼트 레이아웃이 필요할 때 안드로이드가 호출하는 코드
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //  Fragment가 어떤 레이아웃을 사용하는지 안드로이드에 알려줌.
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    public void onSaveInstanceState( Bundle saveInstanceState ) {
+        saveInstanceState.putLong( "workoutId", workoutId );
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        TextView title = (TextView) view.findViewById(R.id.textTitle);
+        Workout workout = Workout.workouts[(int) workoutId];
+        title.setText( workout.getName() );
+        TextView description = (TextView) view.findViewById(R.id.textDescription);
+        description.setText( workout.getDescription() );
+    }
+}
