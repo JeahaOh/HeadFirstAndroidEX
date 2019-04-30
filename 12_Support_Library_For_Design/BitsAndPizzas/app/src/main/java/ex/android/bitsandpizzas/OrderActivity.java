d6ca5228b20f4a8f3905308a762c695753471ff2
JeahaOh <jeaha1217@gmail.com>
2019-05-01 02:28:51 +0900

    .

--- 12_Support_Library_For_Design/BitsAndPizzas/app/src/main/java/ex/android/bitsandpizzas/OrderActivity.java
+++ 12_Support_Library_For_Design/BitsAndPizzas/app/src/main/java/ex/android/bitsandpizzas/OrderActivity.java
@@ -1,9 +1,12 @@
 package ex.android.bitsandpizzas;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -19,6 +22,22 @@ public class OrderActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         //  Up 버튼을 활성화하는 코드. AppBar에 ToolBar를 추가했지만 Up 버튼을 활성화할 때는 ActionBar의 메소드를 사용함.
         actionBar.setDisplayHomeAsUpEnabled( true );
+    }
 
+    public void onClickDone( View view ) {
+        //  FAB을 클릭하면 실행 되는 코드
+        CharSequence text = "Your order has been updated";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+
+        snackbar.setAction("Undo", new View.OnClickListener() {
+           @Override
+           public void onClick( View view ) {
+               //   사용자가 Undo 액션을 클릭하면 실행할 코드
+               Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_SHORT);
+               toast.show();
+           }
+        });
+        snackbar.show();
     }
 }
--- 12_Support_Library_For_Design/READEME.md
+++ 12_Support_Library_For_Design/READEME.md
@@ -4,3 +4,18 @@
 툴바를 접거나 스크롤 되도록 애니메이션으로 만드는 방법을 알아봄.  
 플로팅 액션 버튼으로 가용자 액션을 추가하는 방법을 알아봄.  
 스낵바를 사용해 봄.
+
+## 핵심 정리
+- ViewPager로 Swap 탐색을 활성화할 수 있음.
+- FragmentPagerAdapter를 구현해 ViewPager에 Page 정보를 전달할 수 있음.
+- FragmentPagerAdapter의 getcount()는 얼마나 많은 페이지가 있는지 ViewPager에게 알려줌.  
+  getItem()로 각 페이지가 어떤 Fragment가 표시되어야 하는지 지정함.
+- TabLayout을 구현해 Tab Navigation을 추가함.  
+  Toolbar와 TabLayout을 Layout 코드의 AppBarLayout안에 추가한 다음 Tab Layout을 Activity 코드의 ViewPager로 연결함.
+- TabLayout을 Android Design Support Library에서 제공함.  
+  이 Library는 App에서 Material Design Guide Line을 구현하는데 도움을 줌.
+- CoordinationLayout으로 View 간의 Animation을 조정함.
+- CoordinationLayout이 조정할 수 있는 Scroll 가능한 Content를 NestedScrollView를 사용하여 추가함.
+- 사용자의 ScrollAction에 응답해 접거나 펴는 ToolbarLayout을 사용하는게 좋음.
+- FAB(Floating Action Button)을 이용하면 사용자가 공통 액션이나 중요한 사용자 액션을 쉽게 이용할 수 있도록 제공할 수 있음.
+- Snackbar로 사용자가 상호작용할 수 있는 짧은 메세지를 표시할 수 있음.
